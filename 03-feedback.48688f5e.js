d45da8a572dae581647d6874c3853544a5166ccc
PavloLysiuk <pavel.lysiuk@gmail.com>
2023-07-01 15:50:42 +0000

    Deploying to gh-pages from @ PavloLysiuk/goit-js-hw-08@7aee816d5c5a4908df367737d22a2ce8732d0a8f 🚀

--- 03-feedback.48688f5e.js
+++
@@ -1,2 +0,0 @@
-!function(){var e="undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{},t="object"==typeof e&&e&&e.Object===Object&&e,o="object"==typeof self&&self&&self.Object===Object&&self;t||o||Function("return this")(),Object.prototype.toString;document.querySelector(".feedback-form"),document.querySelector('input[name="email"]'),document.querySelector('textarea[name="message"]')}();
-//# sourceMappingURL=03-feedback.48688f5e.js.map
